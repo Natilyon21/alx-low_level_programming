e96917773a071eb8ab64c166f9e62467a67270eb
Natilyon21 <thelasthockage@gmail.com>
2022-12-09 15:26:05 -0800

    l

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+
+	while
+		(d < 10) {
+			puutchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
