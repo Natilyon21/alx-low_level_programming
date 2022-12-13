2cb350f5026c1f7a15632f4b0947dba6254ab0b4
Natilyon21 <thelasthockage@gmail.com>
2022-12-13 15:48:42 -0800

    l

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for (a = 0; a <= 0; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
@@ -17,7 +17,7 @@ void times_table(void)
 			{
 				if (b == 0)
 				{
-					_putchar('a');
+					_putchar('0');
 				}
 
 				if (b != 0)
