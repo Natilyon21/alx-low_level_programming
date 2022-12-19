91a9e7aecbd1b4b0c7c8716e918e37ee6aceea2b
Natilyon21 <thelasthockage@gmail.com>
2022-12-19 12:43:49 -0800

    l

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,9 +8,9 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; index++)
+	for (i = 0; s[i] != '\0'; i++)
 		;
-	for (i = i - 1; s[i] != '\0'; index--)
+	for (i = i - 1; s[i] != '\0'; i--)
 	{
 		_putchar(s[i]);
 	}
