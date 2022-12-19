ee3bebf012f5f4ed7b7463016f772e9405164058
Natilyon21 <thelasthockage@gmail.com>
2022-12-19 12:40:57 -0800

    l

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: input string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; index++)
+		;
+	for (i = i - 1; s[i] != '\0'; index--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
