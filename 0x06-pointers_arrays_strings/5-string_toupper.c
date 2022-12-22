d4b48a4c24781783b64dd96ae7c7daca57e43041
Natilyon21 <thelasthockage@gmail.com>
2022-12-22 08:17:29 -0800

    l

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @s: string
+ * Return: 0
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] > 96 && s[i] < 123)
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
+}
