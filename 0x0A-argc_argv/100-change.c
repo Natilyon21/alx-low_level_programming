c975d11bc0a5f82a342753ab4e300782223896a5
Natilyon21 <105010511+Natilyon21@users.noreply.github.com>
2023-01-06 13:23:04 -0800

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,88 +1,48 @@
-#include "main"
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <string.h>
-/**
- * checker - checks for valid input
- * @argc: argument count
- * @i: counter for argv[]
- * @j: counter for argv[][]
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
- */
-int checker(int argc, int i, unsigned int j, char *argv[])
-{
-	for (i = 1; i <= argc; i++)
-		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-			if (isdigit(argv[i][j]) == 0)
-				return (1);
-	return (0);
-}
-/**
- * main - Prints the minimum number of coins
- * to make change for an amount of cents.
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success
- */
-int main(int argc, char *argv[])
-{
-	unsigned int cents;
-	int coins;
 
-	cents = coins = 0;
-	if (argc == 2)
-	{
-		if (argv[1][0] == '-')
-			printf("0\n");
-		if (checker(argc, 1, 0, argv) == 0)
-		{
-			cents = atoi(argv[1]);
-			for ( ; cents >= 25; coins++, cents -= 25)
-				;
-			for ( ; cents >= 10; coins++, cents -= 10)
-				;
-			for ( ; cents >= 5; coins++, cents -= 5)
-				;
-			for ( ; cents >= 2; coins++, cents -= 2)
-				;
-			for ( ; cents >= 1; coins++, cents--)
-				;
-			printf("%d\n", coins);
-		}
-	}
-	else
-		printf("Error\n");
-	return (0);
-}
 /**
- * main - Takes in exactly one argument for minimum coin count
+ * main - Prints minumum number of coins to make change
  * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * @argv: Array containing command line arguments
+ *
+ * Return: 0 if success, 1 if not
  */
 int main(int argc, char *argv[])
 {
-
-	int i, coin;
-
-	coin = 0;
+	int i;
+	int cents = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+	int money;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	i = atoi(argv[1]);
-
-	if (i < 0)
+	money = atoi(*(argv + 1));
+	if (money < 0)
+	{
 		printf("0\n");
+	}
 	else
 	{
-		coin = coinConverter(i);
-		printf("%d\n", coin);
+		for (i = 0; i < 5; ++i)
+		{
+			if (money / coins[i])
+			{
+				cents += money / coins[i];
+				/*printf("%d = %d / %d\n", cents, money, coins[i]);*/
+			}
+			if (money % coins[i])
+			{
+				money %= coins[i];
+				/*printf("money = %d\n", money);*/
+			}
+			if (!(money % coins[i]))
+				break;
+		}
+		printf("%d\n", cents);
 	}
 	return (0);
 }
