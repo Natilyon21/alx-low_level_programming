4c51cded4938601c62dac51002f963617d790148
Natilyon21 <105010511+Natilyon21@users.noreply.github.com>
2023-05-31 02:50:49 -0700

    Create 5-island_perimeter.py

---
+++ 0x1C-makefiles /5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Calculates  Island Perimeter"""
+
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors of a grid cell"""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid."""
+
+    perim = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perim += num_water_neighbors(grid, i, j)
+
+    return perim
